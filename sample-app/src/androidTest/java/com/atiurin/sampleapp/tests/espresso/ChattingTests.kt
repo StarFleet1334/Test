8e5a116071eaa7c47678e22a4765f6af4c9f7e58
IliaLataria01 <ilataria@solvd.com>
2024-01-28 21:44:29 +0400

    Updated Injection Environmental variables, last one was deprecated, so used ApplicationProvider

--- sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
+++ sample-app/src/androidTest/java/com/atiurin/sampleapp/tests/espresso/ChattingTests.kt
@@ -1,6 +1,8 @@
 package com.atiurin.sampleapp.tests.espresso
 
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -19,6 +21,7 @@ import com.atiurin.sampleapp.pages.UIMessagePage.toolBarTitle
 import com.atiurin.sampleapp.tests.BaseTest
 import com.atiurin.ultron.extensions.tap
 import io.github.cdimascio.dotenv.Dotenv
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,6 +34,23 @@ class ChattingTests : BaseTest() {
 
     @get:Rule
     val activityTestRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setUp() {
+        // Get the context of the target application
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        // Open the SharedPreferences editor to write values
+        val editor = context.getSharedPreferences("test", Context.MODE_PRIVATE).edit()
+
+        // Put the test values
+        editor.putString("FRIEND_NAME", "Chandler Bing")
+        editor.putString("TEXT_MESSAGE", "Hello Chandler")
+
+        // Apply the changes
+        editor.apply()
+    }
+
 
 
     @Test
@@ -48,29 +68,45 @@ class ChattingTests : BaseTest() {
     fun firstTest() {
         with(UIElementPage) {
 
-            val friend = InstrumentationRegistry.getArguments().getString("FRIEND_NAME", "Rachel Green")
-            val textMessage = InstrumentationRegistry.getArguments().getString("TEXT_MESSAGE", "Hello Rachel")
+            val context = ApplicationProvider.getApplicationContext<Context>()
+            val friend = context.getSharedPreferences("test", Context.MODE_PRIVATE)
+                .getString("FRIEND_NAME", "Rachel Green")
+            val textMessage = context.getSharedPreferences("test", Context.MODE_PRIVATE)
+                .getString("TEXT_MESSAGE", "Hello Rachel")
+
 
             dashboard_menu.isViewDisplayed();
             // Check if User is displayed
-            openChatWithFriend(friend)
+            if (friend != null) {
+                openChatWithFriend(friend)
+            }
             // Open Chat with User
-            openChatWithFriend(friend).tap()
+            if (friend != null) {
+                openChatWithFriend(friend).tap()
+            }
             // Here we check if User is open
-            toolBarTitle(friend).isViewDisplayed()
+            if (friend != null) {
+                toolBarTitle(friend).isViewDisplayed()
+            }
             // We Type Text in Box
-            textInput.typeText(textMessage)
+            if (textMessage != null) {
+                textInput.typeText(textMessage)
+            }
             // Check if send button is displayed
             sendButton.isViewDisplayed()
             // We Assert that input box has what we wrote
-            assert(isMessageDisplayed(textMessage).isViewDisplayed()) {
-                "Message is not displayed in input box"
+            textMessage?.let { isMessageDisplayed(it).isViewDisplayed() }?.let {
+                assert(it) {
+                    "Message is not displayed in input box"
+                }
             }
             // click send button
             sendButton.tap()
             // Now we check if message is displayed  in chat
-            assert(checkMessageInRecyclerView(textMessage)) {
-                "Message is not in chat"
+            textMessage?.let { checkMessageInRecyclerView(it) }?.let {
+                assert(it) {
+                    "Message is not in chat"
+                }
             }
         }
     }
